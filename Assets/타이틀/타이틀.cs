14d4238a8f6a5bb764d05f3cc375f64e9c4e6881
김원겸 <wongyeom4346@gmail.com>
2025-06-24 04:37:04 +0900

    진짜 끝

--- Assets/타이틀/타이틀.cs
+++ Assets/타이틀/타이틀.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class 타이틀 : MonoBehaviour
 {
+    public void GoToTitleScene()
+    {
+        SceneManager.LoadScene("Title");
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player"))
